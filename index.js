b0f6b3c30af91a9b89617f8400a5451ec0971d69
ashiqurriaz <ashiqur15-11943@diu.edu.bd>
2023-01-30 11:33:24 +0600

    add CSS

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ function main(){
         const btn = document.getElementById("changebtn")
 
         btn.addEventListener("click" , function(){
-            const bgcolor = rgbColorGenerator();
-            root.style.backgroundColor = bgcolor;
+            const bgColor = colorRGBgenerator();
+            root.style.backgroundColor = bgColor;
         })
 }
 
-function rgbColorGenerator(){
-    const green = Math.floor(Math.random() * 255);
+
+
+function colorRGBgenerator(){
     const red = Math.floor(Math.random() * 255);
     const blue = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
 
-    return `rgb(${green} , ${red} , ${blue})`
+    return `rgb(${red}, ${blue}, ${green})`
 }
